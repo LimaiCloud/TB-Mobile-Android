b53c548e540b3a5a1c96f8c5ae4a271ead63fe35
w18635782190 <123456>
2018-07-25 10:38:34 +0800

    adapter file

--- app/src/main/java/com/device/limaiyun/thingsboard/adapter/AllAdapter.java
+++ app/src/main/java/com/device/limaiyun/thingsboard/adapter/AllAdapter.java
@@ -1,6 +1,5 @@
 package com.device.limaiyun.thingsboard.adapter;
 
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
