cae82c9a737f9783685ad22b12d91afeefbd6b7c
w18635782190 <123456>
2018-07-25 10:46:52 +0800

    Some utils file,such as read assets file ,time util...

--- app/src/main/java/com/device/limaiyun/thingsboard/utils/TimeUtils.java
+++ app/src/main/java/com/device/limaiyun/thingsboard/utils/TimeUtils.java
@@ -11,8 +11,9 @@ public class TimeUtils {
 
     /*
     * 将时间戳转换为时间
+    * 精确到秒
     */
-    public static String stampToDate(String s){
+    public static String stampToTime(String s){
         String res;
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd HH:mm:ss");
         long lt = new Long(s);
@@ -20,4 +21,32 @@ public class TimeUtils {
         res = simpleDateFormat.format(date);
         return res;
     }
+
+    /*
+     * 将时间戳转换为时间
+     * 精确到日
+     */
+    public static String stampToDate(String s){
+        String res;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd");
+        long lt = new Long(s);
+        Date date = new Date(lt);
+        res = simpleDateFormat.format(date);
+        return res;
+    }
+
+//    public static String dateToStamp(){
+//        String lTime = null;
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        Date date = new Date(System.currentTimeMillis());
+//        String stamp = simpleDateFormat.format(date);
+//        try {
+//            date = simpleDateFormat.parse(stamp);
+//            long time = date.getTime();
+//             lTime = String.valueOf(time);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        return lTime;
+//    }
 }
---
+++ app/src/main/java/com/device/limaiyun/thingsboard/utils/env/Constant.java
@@ -0,0 +1,48 @@
+package com.device.limaiyun.thingsboard.utils.env;
+
+/**
+ * Created by ${Winter} on 2018/7/24.
+ */
+public class Constant {
+
+    public static final String SERVER_URL_STR = "API_SERVE_URL";//基地址
+
+    public static final String LOGIN_URL = "API_AUTH_LOGIN";//登录地址
+
+    public static final String DEVICE_TYPE = "API_DEVICE_TYPE";//设备类型
+
+    public static final String CUSTOMER = "API_CUSTOMER";//用户信息
+
+    public static final String CUSTOMER_DEVICES = "API_CUSTOMER_DEVICES";//用户设备
+
+    public static final String TENANT_DASHBOARDS = "API_TENANT_DASHBOARDS";//看板
+
+    public static final String GET_TIME = "API_GET_TIME";//得到时间戳
+
+    public static final String WANTED = "API_WANTED";//通告
+
+    public static final String WEBSOCKET = "API_WS_URL";//websocket接口
+
+    public static final String USERS = "API_USERS";//用户接口
+
+    public static String API_SERVE_URL = "";
+
+    public static String API_AUTH_LOGIN = "";
+
+    public static String API_DEVICE_TYPE = "";
+
+    public static String API_CUSTOMER = "";
+
+    public static String API_CUSTOMER_DEVICES = "";
+
+    public static String API_TENANT_DASHBOARDS = "";
+
+    public static String API_GET_TIME = "";
+
+    public static String API_WANTED = "";
+
+    public static String API_WS_URL = "";
+
+    public static String API_USERS = "";
+
+}
