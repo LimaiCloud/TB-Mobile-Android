b199b81c8b2454b6f38d8edce40d6c444c4738d6
w18635782190 <123456>
2018-07-25 10:42:10 +0800

    Desalinate the config file

--- app/src/main/java/com/device/limaiyun/thingsboard/base/Configs.java
+++ app/src/main/java/com/device/limaiyun/thingsboard/base/Configs.java
@@ -6,13 +6,16 @@ package com.device.limaiyun.thingsboard.base;
 
 public class Configs {
     public static String BASE_URL = "";
-    public static String API_AUTH_LOGIN = "/api/auth/login";
-    public static String API_DEVICE_TYPES = "/api/tenant/devices?limit=10&textSearch=";
-    public static String API_CUSTOMER="/api/customer/";
-    public static String CUSTOMER_DEVICES="/devices?limit=10&textSearch=";
-    public static String API_TENANT_DASHBOARDS = "/api/tenant/dashboards?limit=";
-    public static String WS_URL = "ws://thingsdevice.limaicloud.com/api/ws/plugins/telemetry?token=";
-    public static String USERS = "http://thingscontacts.limaicloud.com/users?token=";
+//    public static String API_AUTH_LOGIN = "/api/auth/login";
+//    public static String API_DEVICE_TYPES = "/api/tenant/devices?limit=10&textSearch=";
+//    public static String API_CUSTOMER = "/api/customer/";
+//    public static String CUSTOMER_DEVICES = "/devices?limit=10&textSearch=";
+//    public static String API_TENANT_DASHBOARDS = "/api/tenant/dashboards?limit=";
+//    public static String API_GET_TIME = "/api/dashboard/serverTime";
+//    public static String API_WANTED = "http://cms.limaicloud.com/qiyue/wp-json/wp/v2/posts";
+//    public static String WS_URL = "ws://thingsdevice.limaicloud.com/api/ws/plugins/telemetry?token=";
+//    public static String USERS = "http://thingscontacts.limaicloud.com/users?token=";
+//    public static String API_DASHBOARDS="/dashboards";
     public static String Authorization = "X-Authorization";
     public static String BEARER = "Bearer";
     public static String SPACE = " ";
